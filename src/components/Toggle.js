cf9b82ea2d00890da991f31ccd12b1294121b2ce
manymusa <adanalin113@gmail.com>
2022-09-27 16:05:54 -0400

    commit tree 9813fbc0ab6feb62319138db2b3d0bae05026fa7

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [lightSwitch , switchState] = useState(false);
+  function buttonEvent(){
+    switchState((lightSwitch) => !lightSwitch)
+  }
+  let color = lightSwitch ? 'red' : 'white';
+  return <button style = {{background : color }}onClick={buttonEvent}>{lightSwitch ? 'ON': 'OFF'}</button>;
 }
 
 export default Toggle;
